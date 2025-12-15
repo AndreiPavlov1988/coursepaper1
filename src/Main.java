6a24823ca731971fd0671727e7c28c2a5a72e515
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-12-15 20:39:37 +0300

    first2

--- src/Main.java
+++ src/Main.java
@@ -183,32 +183,7 @@ public class Main {
             System.out.println(names[i] + ": " + (added ? "добавлен" : "не добавлен"));
         }
 
-        System.out.println("\n--- 1. Все сотрудники ---");
-        book.printAll();
 
-        System.out.println("\n--- 2. Средняя зарплата ---");
-        System.out.printf("Средняя: %.2f\n", book.averageSalary());
-
-        System.out.println("\n--- 3. Налоги PROPORTIONAL ---");
-        book.calculateTaxes("PROPORTIONAL");
-
-        System.out.println("\n--- 4. Индексация отдела 1 на 10% ---");
-        book.indexSalary(1, 10);
-
-        System.out.println("\n--- 5. Первый в отделе 2 с зарплатой > 200 ---");
-        book.findFirstInDept(2, 200);
-
-        System.out.println("\n--- 6. Первые 3 с зарплатой < 150 ---");
-        book.findWithLowerSalary(150, 3);
-
-        System.out.println("\n--- 7. Проверка наличия ---");
-        Employee test = new Employee("Тест", 1, 200);
-        System.out.println("Есть сотрудник с зарплатой 200: " + book.contains(test));
-
-        System.out.println("\n--- 8. Поиск по ID ---");
-        Employee found = book.getById(3);
-        if (found != null) {
-            System.out.println("Найден: " + found.getName());
         }
     }
 }
